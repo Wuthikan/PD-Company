f6cb363be1ca2c175a0b4947a35af1707c8ae52d
Wuthikan <icewoottikarn@gmail.com>
2017-06-01 07:45:14 +0700

    dd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>PD</title>
+      <meta charset="utf-8">
     <!-- Styles -->
     <!-- <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans|Candal|Alegreya+Sans"> -->
     {!! HTML::style('Mentor/css/font-awesome.min.css') !!}
