fc29dba677d139609cecc3ca9b7a40c296f68d37
Wuthikan <icewoottikarn@gmail.com>
2017-06-08 00:13:04 +0700

    user1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -43,6 +43,12 @@ class UserController extends Controller
      */
     public function store(UserRequest $request)
     {
+      $this->validate($request,[
+        'name' => 'required|string|max:255',
+        'class' => 'required|Integer|max:255',
+        'email' => 'required|string|email|max:255|unique:users',
+        'password' => 'required|string|min:6|confirmed',
+          ]);
         $user =  User::create([
               'name' => $request['name'],
               'email' => $request['email'],
