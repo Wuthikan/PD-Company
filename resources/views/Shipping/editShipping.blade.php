fb980f47ddede22b3ef7b14e510913d2aa30e730
Wuthikan <icewoottikarn@gmail.com>
2017-06-10 23:37:56 +0700

    edit delete thick

--- resources/views/Shipping/editShipping.blade.php
+++ resources/views/Shipping/editShipping.blade.php
@@ -18,7 +18,7 @@
                         <h3>คิวส่งคอนกรีตแผ่นพื้น</h3>
                       @endif
                     @if (empty($date))
-                      <p>วันที่ {{ $shipping->date->formatLocalized('%d/%m/%Y %H') }}
+                      <p>วันที่ {{ $shipping->date->formatLocalized('%d/%m/%Y %h:%m') }}
                         @else
                           <p>วันที่ {{ $date }}  มีคิวที่ลงไว้แล้ว
                              {{ $result }} คิว
