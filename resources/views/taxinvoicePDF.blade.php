c355df218bb14e6a14f93258bd9a431e27a60baa
Wuthikan <icewoottikarn@gmail.com>
2017-06-11 22:39:09 +0700

    sss

--- resources/views/taxinvoicePDF.blade.php
+++ resources/views/taxinvoicePDF.blade.php
@@ -339,9 +339,8 @@
     <tr>
       <td align="left" width="50%" ><font size=3>ผู้รับสินค้า..............................วันที่......./......./.......</font></td>
       <td width="10%" ></td>
-      <td align="left" width="40%" ><font size=3>ผู้รับมอบอำนาจ <img src="{{ url('img/4.jpg') }}" class="img-responsive"></font></td>
+      <td align="left" width="40%" ><font size=3>ผู้รับมอบอำนาจ .............................................</font></td>
     </tr>
-
 </table>
 <hr>
 <script><?php
