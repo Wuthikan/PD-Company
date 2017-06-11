a8840229e86914d209fd468ca6b377030cd5015a
Wuthikan <icewoottikarn@gmail.com>
2017-06-11 23:55:11 +0700

    add pdf signature

--- resources/views/taxinvoicePDF.blade.php
+++ resources/views/taxinvoicePDF.blade.php
@@ -341,7 +341,6 @@
       <td width="10%" ></td>
       <td align="left" width="40%" ><font size=3>ผู้รับมอบอำนาจ <img src="{{ url('img/4.jpg') }}" class="img-responsive"></font></td>
     </tr>
-
 </table>
 <hr>
 <script><?php
