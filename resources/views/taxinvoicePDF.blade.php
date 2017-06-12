5588d53ecea3b67c08f75e52ab3e4fdd27b68afa
Wuthikan <icewoottikarn@gmail.com>
2017-06-12 11:12:45 +0700

    add pdf signature

--- resources/views/taxinvoicePDF.blade.php
+++ resources/views/taxinvoicePDF.blade.php
@@ -338,8 +338,8 @@
     </td></tr>
     <tr>
       <td align="left" width="50%" rowspan="2" ><font size=3>ผู้รับสินค้า..............................วันที่......./......./.......</font></td>
-      <td width="10%" ></td>
-      <td align="left" width="40%" rowspan="2"  ><font size=3>ผู้รับมอบอำนาจ <img height="42" width="200" src="{{ url('img/1.jpg') }}" class="img-responsive"></font></td>
+      <td width="5%" ></td>
+      <td align="left" width="45%" rowspan="2"  ><font size=3>ผู้รับมอบอำนาจ <img height="80" width="200" src="{{ url('img/1.jpg') }}" class="img-responsive"></font></td>
     </tr>
 </table>
 <hr>
